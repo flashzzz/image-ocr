0cf38046c954987a1c417a2c97030cc557b9a05f
Atul Verma <atul.auddy@gmail.com>
2023-01-27 15:37:02 +0530

    added main function

--- image_ocr_code.py
+++ image_ocr_code.py
@@ -4,10 +4,12 @@ import pytesseract
 from glob import glob
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img_paths = glob('*.jpg')
-results=[]
-for i in img_paths:
-    results.append(pytesseract.image_to_string(i, lang='eng'))
+def main():
+    img_paths = glob('*.jpg')
+    # results=[]
+    for i in img_paths:
+        print(pytesseract.image_to_string(i, lang='eng'), end='\n------------------------\n')
 
-for i in results:
-    print(i, end='\n----------------------------------------\n')
+    
+if __name__=='__main__':
+    main()
